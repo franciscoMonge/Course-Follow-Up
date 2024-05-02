fb22b0bf6f54528a881ad281d7a6b40d0d8ab850
sophMcMor <sophyafmm@estudiantec.cr>
2024-05-02 11:21:06 -0600

    Validación flexible horario y fecha

--- course-follow-up/src/components/pages/client/Agregar/agregar_cursos_indiv.jsx
+++ course-follow-up/src/components/pages/client/Agregar/agregar_cursos_indiv.jsx
@@ -29,6 +29,12 @@ const Agregar_Cursos_Indiv = () => {
   const [fechaFinal, setFechaFinal] = useState('');
   const [horarioCurso, setHorarioCurso] = useState('');
 
+  //Modals para errores de fechas
+  const [showWarning, setShowWarning] = useState(false); 
+  const [warningMessage, setWarningMessage] = useState(''); // Mensaje de advertencia
+  const [continueUpdate, setContinueUpdate] = useState(false);
+
+
   useEffect(() => {
     if (cursoSeleccionado) {
       setProfesor(cursoSeleccionado.profesor || '');
@@ -38,27 +44,8 @@ const Agregar_Cursos_Indiv = () => {
     }
   }, [cursoSeleccionado,horario]);
   
-  
-  const handleConfirmar = () => {
-    //Validaciones extra por confimar:
-    //1. Si ya existe un profesor asginado para esas mismas fechas y horario
-    //2. Si ya asignó para ese mismo grupo, otro curso en las mismas fechas
-    console.log("Fecha inicio: ", fechaInicio);
-    console.log("Fecha final: ", fechaFinal);
-    console.log("Horario Curso: ", horarioCurso);
-    console.log("Profesor: ", profesor);
-    //Validar que no haya información en blanco (El profe puede quedar en blanco)
-    if (!fechaInicio || !fechaFinal || !horarioCurso) {
-        console.log("Fecha inicio: ", fechaInicio);
-        console.log("Fecha final: ", fechaFinal);
-        console.log("Horario Curso: ", horarioCurso);
-        console.log("Profesor: ", profesor);
-      alert('Por favor completa todos los campos');
-      return;
-    }
-
-    //Validar que la fecha ingresada coincida con el día del horario
-    
+  //Validar que la fecha ingresada coincida con el día del horario
+  const validarDiaFecha = () => {
     // Convertimos fechas a Date para poder usar getDay()
     const inicioDateObject = new Date(fechaInicio);
     const finDateObject = new Date(fechaFinal);
@@ -79,7 +66,6 @@ const Agregar_Cursos_Indiv = () => {
         return;
       }
       if(diaFin !== 1 && diaFin !== 3){
-        console.log("ERRRORRR fecha");
         toast.error('La fecha de finalización seleccionada es: ' + dias[diaFin] + '. No corresponde a Lunes ni Miércoles.');
         return;
       }
@@ -97,54 +83,82 @@ const Agregar_Cursos_Indiv = () => {
         return;
       }
     }
-    
 
-    // Validar que las fechas tengan concordancia
-    if (fechaInicio > fechaFinal) {
-      alert('La fecha de inicio debe ser anterior a la fecha final');
-      return;
-    }
+  }; //Fin validar dia fecha
 
-  
-    // Validar que haya una distancia mínima de 1 mes entre las fechas
+  const validarDistanciaFechas = () =>{
+    // Validar que haya una distancia mínima de 1 mes entre las fechas(OPCIONAL)
     const fechaInicioObj = new Date(fechaInicio);
     const fechaFinalObj = new Date(fechaFinal);
     const diferenciaMeses = (fechaFinalObj.getFullYear() - fechaInicioObj.getFullYear()) * 12 + fechaFinalObj.getMonth() - fechaInicioObj.getMonth();
-    if (diferenciaMeses < 1) {
-      alert('Debe haber una distancia mínima de 1 mes entre la fecha de inicio y la fecha final');
-      return;
-    }
-  
-    // Validar que el horario del curso coincida con el horario del grupo
-    if (horarioCurso !== horario) {
-      alert('El horario del curso debe coincidir con el horario del grupo');
-      return;
-    }
     
-    // Llamar a la API para actualizar los cursos
-    axios.post('http://localhost:3001/actualizarCursos', {
-        idGrupo: idGrupo, //En mi MYSQL las inserciones empiezan en 1 
-        idCurso: idCurso,//En mi MYSQL las inserciones empiezan en 1,por eso hay que sumarle 1
-        fechaInicio: fechaInicio,
-        fechaFinal: fechaFinal,
-        profesor: profesor,
-        horario: horarioCurso
-    })
-    .then(response => {
-        console.log('Curso actualizado correctamente:', response.data);
-        alert("Curso actualizado correctamente");
-        navigate('/AgregarCursos', { state: { idGrupo, numero, horario  } });
-        // Aquí puedes realizar otras acciones después de actualizar los cursos, como mostrar un mensaje de éxito, etc.
-    })
-    .catch(error => {
-        console.error('Error al actualizar curso:', error);
-        alert("Error al actualizar el curso");
-        // Aquí puedes manejar el error, mostrar un mensaje de error al usuario, etc.
-    });
+    if (diferenciaMeses < 1) {
+      setWarningMessage('Debe haber una distancia mínima de 1 mes entre la fecha de inicio y la fecha final');
+      setShowWarning(true);
+      return;
+    }
+  } //Fin validar distancia fechas
 
+  //Validar que el horario del curso coincida con el horario del grupo (OPCIONAL QUE SE CUMPLA)
+  const validarHorarioCursoGrupo = () => {
+    if (horarioCurso !== horario) {
+      setWarningMessage('El horario del curso debe coincidir con el horario del grupo');
+      setShowWarning(true);
+      return;
+    }
+  } //Fin validar horario curso grupo
 
+  const handleConfirmar = () => {
+    //V#1.Validar que no haya información en blanco (El profe puede quedar en blanco)
+    if (!fechaInicio || !fechaFinal || !horarioCurso) {
+        console.log("Fecha inicio: ", fechaInicio);
+        console.log("Fecha final: ", fechaFinal);
+        console.log("Horario Curso: ", horarioCurso);
+        console.log("Profesor: ", profesor);
+      toast.error('Por favor completa todos los campos');
+      return;
+    }
+    //V#2.Validar que la fecha ingresada coincida con el día del horario
+    validarDiaFecha();     
+    //V#3.Validar que las fechas tengan concordancia
+    if (fechaInicio > fechaFinal) {
+      toast.error('La fecha de inicio debe ser anterior a la fecha final');
+      return;
+    }
+    // V#4. Validar que haya una distancia mínima de 1 mes entre las fechas (OPCIONAL QUE SE CUMPLA)
+    validarDistanciaFechas();
+    // V#5. Validar que el horario del curso coincida con el horario del grupo (OPCIONAL QUE SE CUMPLA)
+    validarHorarioCursoGrupo();
   };
+
+  useEffect(() => {
+    if (showWarning && !continueUpdate) {
+      setShowWarning(true);
+      return; // Detener la ejecución aquí para esperar la confirmación del usuario
+    }
   
+    // Si no hay advertencias o el usuario ha confirmado continuar, continuar con la actualización
+    if (continueUpdate) {
+      console.log("Horario en update:",horarioCurso);
+      axios.post('http://localhost:3001/actualizarCursos', {
+          idGrupo: idGrupo,
+          idCurso: idCurso,
+          fechaInicio: fechaInicio,
+          fechaFinal: fechaFinal,
+          profesor: profesor,
+          horario: horarioCurso
+      })
+      .then(response => {
+          console.log('Curso actualizado correctamente:', response.data);
+          alert("Curso actualizado correctamente");
+          navigate('/AgregarCursos', { state: { idGrupo, numero, horario } });
+      })
+      .catch(error => {
+          console.error('Error al actualizar curso:', error);
+          alert("Error al actualizar el curso");
+      });
+    }
+  }, [showWarning, continueUpdate]);
 
     const handleChange = (e) => {
         // console.log("Profesor: ", profesor);
@@ -154,97 +168,122 @@ const Agregar_Cursos_Indiv = () => {
         setHorarioCurso(e.target.value);
     };
 
-  const handleBack = () => {
-    
-    navigate('/AgregarCursos', { state: { idGrupo, numero, horario  } });
-  };
+    const handleBack = () => {
+      
+      navigate('/AgregarCursos', { state: { idGrupo, numero, horario  } });
+    };
+
+    const handleContinue = () => {
+      setShowWarning(false);
+      setContinueUpdate(true);
+    };
+
+    const handleCerrarModal = () =>{
+      setShowWarning(false);
+      setContinueUpdate(false);
+    }
 
 
-  return (
 
-<div>
-  <Navbar />
-  <div className="container" style={{ paddingTop: '80px' }}>
-    <ToastContainer position="top-center"/>
-    <h3>Agregar Planificación</h3>
-    <div className="row">
-      <div className="col">
-        <div className="form-group">
-        <span className="badge bg-light text-dark">
-          <h5>Grupo:</h5>
-          <h5>{numero}</h5> 
- 
-        </span>
-        </div>
-      </div>
-      <div className="col">
-        <div className="form-group">
-        <span className="badge bg-light text-dark">
-          <h5>Horario del grupo:</h5>
-          <h5>{horario}</h5>  
-        </span>
-        </div>
-      </div>
-    </div>
-    <br></br>
-
-    <div className="row">
-      <div className="col-md-12 mb-12">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{cursoSeleccionado.nombre_curso}</h5>
-            <div className="form-group">
-              <label>Profesor:</label>
-              <input
-                type="text"
-                className="form-control"
-                value={profesor}
-                onChange={(e) => setProfesor(e.target.value)}
-              />
+    return (
+      <div>
+        <Navbar />
+        <div className="container" style={{ paddingTop: '80px' }}>
+          <ToastContainer position="top-center"/>
+          <h3>Agregar Planificación</h3>
+          {showWarning && (
+            <div className="modal" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Advertencia</h5>
+                    <button type="button" className="btn-close" onClick={handleCerrarModal}></button>
+                  </div>
+                  <div className="modal-body">
+                    {warningMessage}
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" onClick={handleCerrarModal}>Cerrar</button>
+                    <button type="button" className="btn btn-primary" onClick={handleContinue}>Continuar</button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='form-group'>
-              <label>Fecha Inicio:</label>
-              <input
-                type="date"
-                className="form-control"
-                value={fechaInicio}
-                onChange={(e) => setFechaInicio(e.target.value)}
-              />
+          )}
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <span className="badge bg-light text-dark">
+                  <h5>Grupo:</h5>
+                  <h5>{numero}</h5> 
+                </span>
+              </div>
             </div>
-            <div className='form-group'>
-              <label>Fecha Final:</label>
-              <input
-                type="date"
-                className="form-control"
-                value={fechaFinal}
-                onChange={(e) => setFechaFinal(e.target.value)}
-              />
+            <div className="col">
+              <div className="form-group">
+                <span className="badge bg-light text-dark">
+                  <h5>Horario del grupo:</h5>
+                  <h5>{horario}</h5>  
+                </span>
+              </div>
             </div>
-            <div className="form-group">
-              <label>Horario:</label>
-                <select 
+          </div>
+          <br></br>
+          <div className="row">
+            <div className="col-md-12 mb-12">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{cursoSeleccionado.nombre_curso}</h5>
+                  <div className="form-group">
+                    <label>Profesor:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={profesor}
+                      onChange={(e) => setProfesor(e.target.value)}
+                    />
+                  </div>
+                  <div className='form-group'>
+                    <label>Fecha Inicio:</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      value={fechaInicio}
+                      onChange={(e) => setFechaInicio(e.target.value)}
+                    />
+                  </div>
+                  <div className='form-group'>
+                    <label>Fecha Final:</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      value={fechaFinal}
+                      onChange={(e) => setFechaFinal(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Horario:</label>
+                      <select 
                         className="form-select form-select-sm" 
                         aria-label=".form-select-sm example" 
                         onChange={handleChange}
                         value={horarioCurso || ''}>
                         <option value="L-M">L-M</option>
                         <option value="K-J">K-J</option>
-                </select>
+                      </select>
+                  </div>
+                  <button className="btn btn-success m-4" onClick={handleConfirmar}>Confirmar cambios</button>
+                </div>
+              </div>
             </div>
-            <button className="btn btn-success m-4" onClick={handleConfirmar}>Confirmar cambios</button>
+          </div>
+          <div className="m-3">
+            <hr />
+            <button className="btn btn-danger m-4" onClick={handleBack}>Volver</button>
           </div>
         </div>
       </div>
-    </div>
-
-    <div className="m-3">
-      <hr />
-      <button className="btn btn-danger m-4" onClick={handleBack}>Volver</button>
-    </div>
-  </div>
-</div>
-
-  );
-};
-
-export default Agregar_Cursos_Indiv;
+    );
+  };
+  
+  export default Agregar_Cursos_Indiv;
