89b6df9b97ac6df9d14e140b44d8ac1b330bcc20
Mariana <mfernandezmar3@gmail.com>
2024-05-16 17:18:46 -0600

    Cambios en Mi Cuenta

--- course-follow-up/src/components/pages/client/Cuenta/mi_cuenta.jsx
+++ course-follow-up/src/components/pages/client/Cuenta/mi_cuenta.jsx
@@ -1,39 +1,81 @@
 import Navbar from "../../shared/navbar";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
 
 function MiCuenta () {
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleBack = () =>{
         navigate('/MainPage');
     };
 
+    const handleEdit = () => {
+        navigate('/EditarCuenta');
+    };
+
+    const handleAccount = () => {
+        navigate('/GestionarAdministradores');
+    };
+
     return(
         <div>
             <Navbar/>           
-        <div style={{ marginTop: '100px'}}>
+        <div style={{ marginTop: '80px'}}>
             <h1 style={{ color: 'white'}} >Mi Cuenta</h1>
+            
             <div className="card m-4 p-4" style={{ textAlign: 'left'}}>
-                <br />
-                <h5>Nombre: Mariana</h5>
-                <br />
-                <h5>Apellido: Fernández </h5>
-                <br />
-                <h5>Dirección de correo: marianafdzm@estudiantec.cr </h5>
-                <br />
-                <h5>Contraseña: <input type='password' value='1234' readOnly style={{ backgroundColor: 'transparent', color: 'black'}}/></h5>
+                
+                <div className="form-group">
+                    <br/>
+                    <h5 style={{ display: 'inline'}}>Nombre: </h5>
+                    <label style={{ display: 'inline', fontSize: 'larger'}}> Mariana</label>
+                </div>
+                
+                <div className="form-group">
+                    <br/>
+                    <h5 style={{ display: 'inline'}}>Apellido: </h5>
+                    <label style={{ display: 'inline', fontSize: 'larger'}}> Fernández</label>
+                </div>
+                
+                <div className="form-group">
+                    <br/>
+                    <h5 style={{ display: 'inline'}}>Dirección de correo: </h5>
+                    <label style={{ display: 'inline', fontSize: 'larger'}}> marianafdmz@estudiantec.cr</label>
+                </div>
+                
+                <div className="form-group">
+                    <br/>
+                    <h5 >Contraseña: </h5>
+                    <input
+                        type= {showPassword ? "text" : "password"} 
+                        className="form-control m-2" 
+                        value= '123445'
+                        readOnly
+                    />
+                    <button 
+                        type="button" 
+                        className="btn position-absolute end-0 top-50 translate-middle-y"
+                        onClick={() => setShowPassword(!showPassword)}
+                        style={{ marginRight: "15px", marginTop: '70px' }}
+                        >
+                            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                    </button>
+                </div>
 
                 <div style={{ textAlign: 'right'}}>
-                    <button className="btn-link" style={{ backgroundColor: '#092D4E'}}>
+                    <br/>
+                    <button className="btn-link" style={{ backgroundColor: '#092D4E'}} onClick={handleEdit}>
                         <FontAwesomeIcon icon={faEdit} className="mr-2" />
                     </button>
                 </div>
             </div>
             
-            <div  style={{ textAlign: 'left' }}>
-                <button className="btn btn-primary">Gestionar Administradores</button>
+            <div  style={{ textAlign: 'center' }}>
+                <button className="btn btn-primary" onClick={handleAccount}>Gestionar Administradores</button>
                 <br />
                 <button className="btn btn-danger">Eliminar Cuenta</button>
                 <br />
